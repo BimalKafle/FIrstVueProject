1a6beb951fab5a4807d6c0c0808ee3ba2398af29
bimal <bimal@OKICOM>
2023-07-10 11:13:52 +0900

    initial push

---
+++ firstvueproject/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+ <ModalComponent :header="header" :component="component" :theme="theme"/>
+ 
+</template>
+
+<script>
+
+import ModalComponent from './components/ModalComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    ModalComponent
+  },
+  data(){
+    return{
+      header:"Sign Up",
+      component:"Please sign in to continue",
+      theme:"sale"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ firstvueproject/src/components/ModalComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{sale: theme==='sale'}">
+            <div class="modal-header" >
+                <h2>{{header}}</h2>
+            </div>
+            <hr>
+            <p>{{component}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props:['header','component','theme']
+}
+</script>
+
+<style >
+.modal{
+    width: 400px;
+    padding:20px;
+    margin:100px auto;
+    background:white;
+    border-radius:10px;
+}
+
+.modal.sale{
+    color: white;
+    background: red;
+}
+
+.modal-header h2{
+    font-size: 10;
+    
+    margin: 0;
+    text-align: start;
+}
+
+.backdrop{
+    width:100%;
+    height:100%;
+    top:0;
+    position:fixed;
+    background:black;
+}
+</style>
